c7da3ba09347bc834e02953bc07403ad8be473b2
XabierAC <126728551+XabierAC@users.noreply.github.com>
2024-11-26 17:52:09 +0100

    Update FicherosGestion.java
    Primeras modificaciones de tarea practica 1 de la UD4

---
+++ Unidad04/FicherosGestion.java
@@ -0,0 +1,27 @@
+public class FicherosGestion {
+  //Metodo para leer y escribir ficheros y trabajar con los datos del fichero
+  public static void main (String[] args){
+    ficheroEntrada();
+    ficheroSalida();
+    resultado();
+  }
+  //Metodo que sirve para pedirle al usuario el nombre del archivo de entrada
+  public static void ficheroEntrada(){
+    
+  }
+  //Metodo que sirve para crear o verificar el archivo de salida a utilizar
+  public static void ficheroSalida(){
+    
+  }
+  //Metodo que llama a los metodos para mostrar lo que hay en los archivos
+  public static void resultado(){
+    lineasCorrectas();
+    lineasIncorrectas();
+  }
+  public static void lineasCorrectas(){
+    
+  }
+  public static void lineasIncorrectas(){
+    
+  }
+}
