2895f62d53ec829cbd8e12fd2ea54e4a7e163f0f
XabierAC <xabieracdeveloper@gmail.com>
2024-12-12 11:50:44 +0100

    Reads and creates File, need data work of datos.txt

--- Unidad04/FicherosGestion.java
+++ Unidad04/FicherosGestion.java
@@ -1,27 +1,29 @@
+//Archivo de la Tarea Práctica 1 de Prog04
+import java.util.*;
+import java.io.*;
+
 public class FicherosGestion {
-  //Metodo para leer y escribir ficheros y trabajar con los datos del fichero
-  public static void main (String[] args){
-    ficheroEntrada();
-    ficheroSalida();
-    resultado();
+  //Programa para coger los datos de un fichero, pasarlos a otro y generar nuevos datos a partir de los del primer fichero
+  public static void main (String[] args) throws FileNotFoundException{
+    File ficheroEntrada = null;
+    Scanner teclado = new Scanner(System.in);
+    boolean sePuedeLeer = false;
+    //Pedimos al usuario que nos diga que archivo queremos leer, en caso de que no encuentre el archivo lo indicamos y le pedimos un nuevo archivo
+    do{
+      System.out.println("Introduce el nombre del Fichero de entrada:");
+      ficheroEntrada = new File("/Users/xabierac/Developer/GitHub/FPBirtlh/Unidad04/" + teclado.nextLine());
+      if (ficheroEntrada.canRead()) {
+        sePuedeLeer = true;
+      } else {
+        System.out.println("Fichero no encontrado. Inténtalo otra vez");
+      }
+    }while (!sePuedeLeer);   
+    //Creamos el archivo nuevo en el que vamos a guardar los datos con los que hemos trabajado
+    System.out.println("Introduce el nombre del Fichero de salida");
+    File ficheroSalida = new File("/Users/xabierac/Developer/GitHub/FPBirtlh/Unidad04/" + teclado.nextLine());  
+    PrintStream escribirFichero = new PrintStream(ficheroSalida);
   }
-  //Metodo que sirve para pedirle al usuario el nombre del archivo de entrada
-  public static void ficheroEntrada(){
+  public static void tratamientoDatos() {
     
   }
-  //Metodo que sirve para crear o verificar el archivo de salida a utilizar
-  public static void ficheroSalida(){
-    
-  }
-  //Metodo que llama a los metodos para mostrar lo que hay en los archivos
-  public static void resultado(){
-    lineasCorrectas();
-    lineasIncorrectas();
-  }
-  public static void lineasCorrectas(){
-    
-  }
-  public static void lineasIncorrectas(){
-    
-  }
-}
+} 
