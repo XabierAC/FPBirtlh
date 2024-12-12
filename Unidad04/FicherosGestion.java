72a165d263e53fa9923c14a1897893ea6c6fc007
XabierAC <126728551+XabierAC@users.noreply.github.com>
2024-12-12 15:23:46 +0100

    Close of FicherosGestion
    End of the task, needs to be checked on vsc

--- Unidad04/FicherosGestion.java
+++ Unidad04/FicherosGestion.java
@@ -23,6 +23,8 @@ public class FicherosGestion {
     File ficheroSalida = new File("/Users/xabierac/Developer/GitHub/FPBirtlh/Unidad04/" + teclado.nextLine());  
     PrintStream escribirFichero = new PrintStream(ficheroSalida);
     tratamientoDatos(ficheroEntrada, escribirFichero);
+    escribirFichero.close();
+    teclado.close();
   }
   public static void tratamientoDatos(File ficheroEntrada,PrintStream escribirFichero) throws FileNotFoundException{
     Scanner leerDatos = new Scanner(ficheroEntrada);
@@ -57,8 +59,8 @@ public class FicherosGestion {
     }
     promedio = suma / cont;
     escribirEnNuevoFichero(suma, promedio, mayor, menor, validados, palabra1, palabra2, escribirFichero);
-    mostrarPorPantalla();
-
+    mostrarPorPantalla(suma, promedio, mayor, menor, validados, palabra1, palabra2);
+    leerDatos.close();
   }
   public static void escribirEnNuevoFichero(int suma, double promedio, int mayor, int menor, int validados, String palabra1, String palabra2, PrintStream escribirFichero){
     escribirFichero.println("Lineas correctas:");
@@ -71,8 +73,18 @@ public class FicherosGestion {
     escribirFichero.println("\t Linea inválida: " + palabra1);
     escribirFichero.println("\t Linea inválida: " + palabra2);
   }
-  public static void mostrarPorPantalla(){
-    
+  public static void mostrarPorPantalla(int suma, double promedio, int mayor, int menor, int calidados, String palabra1, String palabra 2){
+    PrintStream escribirPantalla = System.out;
+    escribirFichero.println("Lineas correctas:");
+    escribirFichero.println("\t Suma: " + suma);
+    escribirFichero.println("\t Promedio: " + promedio);
+    escribirFichero.println("\t Mayor: " + mayor);
+    escribirFichero.println("\t Menor: " + menor);
+    escribirFichero.println("\t Número validados: " + validados);
+    escribirFichero.println("Lineas incorrectas:");
+    escribirFichero.println("\t Linea inválida: " + palabra1);
+    escribirFichero.println("\t Linea inválida: " + palabra2);
+    escribirPantalla.close();
   }
 /***Suma de todos los números (Suma)
 La media de todos los números enteros (Promedio).
