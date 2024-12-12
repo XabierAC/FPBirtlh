afdce90aaccc4be190e8abc64851f130cefa2abd
XabierAC <126728551+XabierAC@users.noreply.github.com>
2024-12-12 18:04:01 +0100

    Create CuentosLocos.java

---
+++ Unidad04/CuentosLocos.java
@@ -0,0 +1,42 @@
+
+public class CuentosLocos3 {
+    
+    public static void main(String[] args) {
+    
+        
+    }
+    
+    public static void presentacion() {
+		System.out.println("Bienvenidos y bienvenidas al juego de los cuentos locos.\n"
+				+ "El programa te pedirá que introduzcas una serie de palabras\n"
+				+ "que se utilizarán para completar una historia.\n" + "El resultado se guardará en un fichero.\n"
+				+ "Puedes leer esas historias siempre que quieras.\n");		
+	}
+	
+	/*
+	 * Método que crea el cuento loco. 
+	 * Pide el nombre del fichero de entrada con el cuento loco y el de salida identifica las palabras a completar,
+	 * las pide por teclado y escribe el cuento en el fichero de salida.
+	 * Se gestiona cualquier excepción que pueda ocurrir mientras se lee o escribe el fichero.
+	 * Las excepciones se tratarán en el mismo métood sin lanzarlas mediante throws. 
+	 * @Param: Scanner teclado para leer valores introducidos por teclado No devuelve ningún valor.
+	 * @Return: No devuelve ningún valor.
+	 */
+	public static void crearCuento(Scanner teclado) {
+	
+	    
+	}
+	
+	/*
+	 * Método que muestra el contenido de un fichero. 
+	 * Pide el nombre del fichero hasta que éste sea válido, lo lee línea a línea y lo visualiza por consola.
+	 * Se gestiona cualquier excepción que pueda ocurrir mientras se lee el fichero.
+	 * Las excepciones se tratarán en el mismo métood sin lanzarlas mediante throws. 
+	 * @Param: Scanner teclado para leer valores introducidos por teclado 
+	 * @Return: No devuelve ningún valor.
+	 */
+	public static void verCuento(Scanner teclado) {
+	
+	    
+	}
+}
