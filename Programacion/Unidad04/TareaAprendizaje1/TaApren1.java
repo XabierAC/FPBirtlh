05f4b46a1818b5310a9845908d93964f0c9c2ba7
XabierAC <xabier139@gmail.com>
2024-12-26 23:53:15 +0100

    Actualizacion Tarea aprendizaje 1 UD04 y creacion carpeta UD05

---
+++ Programacion/Unidad04/TareaAprendizaje1/TaApren1.java
@@ -0,0 +1,50 @@
+package TareaAprendizaje1;
+import java.util.*;
+import java.io.*;
+
+public class TaApren1 {
+    public static void main (String[] args) throws FileNotFoundException{
+        File fichero = new File("/Users/xabierac/Developer/Formacion Developer All/FPBirtlh/Programacion/Unidad04/datos.txt");
+        Scanner leerFichero = new Scanner(fichero);
+        int total = 0;
+        double promedio = 0;
+        int mayor = 0;
+        int menor = 0;
+        int validos = 0;
+        String palabras = "";
+        
+        while (leerFichero.hasNextLine()) {
+            if (leerFichero.hasNextInt()) {
+                int numero = Integer.parseInt(leerFichero.nextLine());
+                total += numero;
+                validos ++;
+                if (numero > mayor){
+                    mayor = numero;
+                }
+                if (numero < menor || menor == 0) {
+                    menor = numero;
+                }
+            } else {
+                if (palabras == ""){
+                    palabras = leerFichero.nextLine();
+                }else {
+                    palabras = palabras + "/" + leerFichero.nextLine();
+                }
+            }
+        }
+        promedio = total / validos;
+        System.out.println("Total: " + total);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+        System.out.println("Validos: " + validos);
+        Scanner palabrasInvalidas = new Scanner(palabras);
+        palabrasInvalidas.useDelimiter("/");
+        while (palabrasInvalidas.hasNext()){
+            System.out.println(palabrasInvalidas.next());
+        }
+        leerFichero.close();
+        palabrasInvalidas.close();
+    }
+}
+
