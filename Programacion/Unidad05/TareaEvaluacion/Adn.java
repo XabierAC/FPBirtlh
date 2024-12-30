dc09e3c7737b9eb74b2b62c89b5b996baca5d395
XabierAC <xabier139@gmail.com>
2024-12-30 10:25:02 +0100

    Calcula la cantidad de cada tipo de nucleolitos correctamente y lo reinicia tras finalizar con esa cadena.

--- Programacion/Unidad05/TareaEvaluacion/Adn.java
+++ Programacion/Unidad05/TareaEvaluacion/Adn.java
@@ -10,19 +10,55 @@ public class Adn {
     final double masaBasura = 100.000;
     final int minCodones = 5;
     final int portentajeCitosinaGuanina = 30;
-    final int tiposNucleolitos = 4;
+    final static int tiposNucleolitos = 4;
     final int nucleolitosCodon = 3;
 
-    int[] nucleolitos = new int[tiposNucleolitos];
+    static int[] nucleolitos = new int[tiposNucleolitos];
     double[] masaNucleolitos = new double[4];
     String[] listaCodones;
-
+    
     /* Programa que sirve para generar información importante sobre nucleolitos, simulando que trabaja como los biólogos computacioneles. 
      * Mostrará cuantas veces aparece cada tipo de nucleolito, el porcentaje de masa correspondiente, los codones, indicarña si la 
      * secuencia podría ser un gen que codifica la proteina o no.
      */
     public static void main (String[] args) {
-    
+        Scanner scannerLectura = null;
+        PrintStream escrituraLineas = null;
+
+        try {
+            File ficheroLectura = new File("/Users/xabierac/Developer/Formacion Developer All/FPBirtlh/Programacion/Unidad05/TareaEvaluacion/dna.txt");
+            scannerLectura = new Scanner(ficheroLectura);
+            while (scannerLectura.hasNextLine()) {
+                String descripcionCadena = scannerLectura.nextLine();
+                String cadenaAdn = scannerLectura.nextLine().toUpperCase();
+                // Trabamos para calcular todos los datos necesarios de la cadena y damos formato por pantalla y por fichero.
+                calculoTipoNucleolitos(cadenaAdn, nucleolitos);
+                System.out.println(Arrays.toString(nucleolitos));
+                //calculoMasaNucleolitos(nucleolitos, cadenaAdn);
+                reinicioArrayNucleolitos(nucleolitos);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se ha localizado el archivo que queria leer. " + e.getMessage());
+        } catch (Exception e){
+            System.out.println("Ha habido un error inesperado." + e.getMessage());
+        }
+        scannerLectura.close();
+    }
+
+    public static void calculoTipoNucleolitos(String cadenaAdn, int[] nucleolitos){
+        char tipoNucleolitoChar;
+        for (int i = 0; i < cadenaAdn.length(); i++) {
+            tipoNucleolitoChar = cadenaAdn.charAt(i);
+            if (tipoNucleolitoChar == 'A') {
+                nucleolitos[0]++;
+            } else if (tipoNucleolitoChar == 'C') {
+                nucleolitos[1]++;
+            } else if (tipoNucleolitoChar == 'G') {
+                nucleolitos[2]++;
+            } else  if (tipoNucleolitoChar == 'T'){
+                nucleolitos[3]++;
+            }
+        }
     }
 
     /* Método para escribir todos los resultados obtenidos en el fichero.
@@ -44,7 +80,7 @@ public class Adn {
      * @param: array nucleolitos con las veces que aparece cada tipo de nucleolito
      * @return: la masa total de los nucleolitos
      */
-    public static double calculoMasaNucleolitos(double[] nucleolitos){
+    public static double calculoMasaNucleolitos(int[] nucleolitos, String cadenaAdn){
         double masaTotal = 0;
 
         return masaTotal;
@@ -58,13 +94,13 @@ public class Adn {
         
     }
 
-    /* Método para calcular los codones de la cadena de ADN
+    /* Método para calcular los codones de la cadena de ADN y almacenarlos como tal en un array.
      * @param: cadenaAdn con la secuencia de ADN
      * @return: no devuelve ningún valor.
      */
     public static int calculoCodones(String cadenaAdn){
         int contador = 0;
-
+        // La cadena de ADN puede contener giones, los cuales cuentan para la suma, pero no para el array de muestra de codones.
         return contador;
 
     }
@@ -75,7 +111,12 @@ public class Adn {
      */
     public static boolean comprobarEsProteina(){
         boolean esProteina = false;
-
+        // Comienza por ATG, finaliza en TAA-TAG-TGA, contiene al menos 5 codones, C + G >= 30%
         return esProteina;
+    }
+    public static void reinicioArrayNucleolitos(int[] nucleolitos) {
+        for (int i =0; i < nucleolitos.length; i++) {
+            nucleolitos[i] = 0;
+        }
     }
 }
